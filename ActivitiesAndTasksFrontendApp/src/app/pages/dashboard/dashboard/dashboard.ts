c35863ea9428b6187dc26d1e86fe47b325b23cde
Sudhakara Rao Kilaparthi <sudhakara@blacklake.in>
2025-12-08 04:21:11 +0530

    User full name displaying in dashboard

--- ActivitiesAndTasksFrontendApp/src/app/pages/dashboard/dashboard/dashboard.ts
+++ ActivitiesAndTasksFrontendApp/src/app/pages/dashboard/dashboard/dashboard.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { User } from '../../../core/models/user.model';
+import { StorageService } from '../../../core/services/storage.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +8,11 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
+  storageService = inject(StorageService);
+  user = signal<User | null>(null);
 
+  ngOnInit(): void {
+    this.user.set(this.storageService.getUserDetails()); // Initialization logic can be added here
+  }
 }
